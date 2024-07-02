7e228a848e292cc22aebcaab4e16e6db87e41e18
Michal Wesolowski <michal.wesolov@gmail.com>
2024-07-02 10:08:56 +0200

    implemented adding groups to group

--- mynotes/api/serializers/group_serializer.py
+++ mynotes/api/serializers/group_serializer.py
@@ -14,14 +14,37 @@ class GroupDetailsSerializer(serializers.ModelSerializer):
         }
 
 class GroupCreateUpdateSerializer(serializers.ModelSerializer):
-    members_ids = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all(), required=False)
-    owners_ids = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all(), required=False)
-    members = UserDetailsSerializer(many=True, read_only=True)
     owners = UserDetailsSerializer(many=True, read_only=True)
+    members = UserDetailsSerializer(many=True, read_only=True)
+    members_ids = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all(), required=False, write_only=True)
+    owners_ids = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all(), required=False, write_only=True)
+    member_groups_ids = serializers.PrimaryKeyRelatedField(many=True, queryset=Group.objects.all(), required=False, write_only=True)
+    owner_groups_ids = serializers.PrimaryKeyRelatedField(many=True, queryset=Group.objects.all(), required=False, write_only=True)
+
 
     class Meta:
         model = Group
-        fields = ['id', 'name', 'image', 'members', 'owners', 'members_ids', 'owners_ids']
+        fields = ['id', 'name', 'image', 'members', 'owners', 'members_ids', 'owners_ids', 'member_groups_ids', 'owner_groups_ids']
         extra_kwargs = {
             'id': {'read_only': True}
         }
+
+    def create(self, validated_data):
+        members_ids = validated_data.pop('members_ids', [])
+        owners_ids = validated_data.pop('owners_ids', [])
+        member_groups_ids = validated_data.pop('member_groups_ids', [])
+        owner_groups_ids = validated_data.pop('owner_groups_ids', [])
+        group = Group.objects.create(
+            name=validated_data.get('name'),
+        )
+
+        group.members.set(members_ids)
+        group.owners.set(owners_ids)
+
+        for group in member_groups_ids:
+            group.members.add(*group.members.all())
+
+        for group in owner_groups_ids:
+            group.owners.add(*group.members.all())
+
+        return group
